4bd121549168c1ecff7cdda43309fb85ff35bab2
simonlovesyou <simonljohansson92@gmail.com>
2016-04-11 23:46:19 +0200

    Fix throw error instead of returning

--- src/js/background/lib/launch.js
+++ src/js/background/lib/launch.js
@@ -3,14 +3,14 @@ import {isAbsolute} from 'path';
 
 function launch(path, options = {}) {
   if(!isAbsolute(path)) {
-    return new Error('Application path "%s" is not absolute.', path);
+    throw new Error('Application path "%s" is not absolute.', path);
   }
   if(!options instanceof Object) {
-    return new Error('Option passed to launch must be an instance of Object, got ', typeof options);
+    throw new Error('Option passed to launch must be an instance of Object, got ', typeof options);
   }
 
   if(!options.detached instanceof Boolean) {
-    return new Error('Option key \'detached\' must an instance of Boolean, got ', typeof options.detached);
+    throw new Error('Option key \'detached\' must an instance of Boolean, got ', typeof options.detached);
   }
 
   return exec(path, {detached: options.detached || true}, (err, stdout, stderr) => {
