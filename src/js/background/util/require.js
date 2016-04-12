241cfc793668d3f65701ec02da79ef0698a3a5b7
simonlovesyou <simonljohansson92@gmail.com>
2016-04-12 22:35:09 +0200

    Add Todo to handle when user requests a module that has not been bundled

--- src/js/background/util/require.js
+++ src/js/background/util/require.js
@@ -1,4 +1,8 @@
-function setUserRequire (lib) {
+/** TODO:
+ * When lib[module] returns undefined, look for a file relative to the script that's being executed and return that file.
+ */
+
+function setUserRequire (lib, dir) {
   if (!lib instanceof Object) {
     throw new Error('User lib is not an object');
   }
