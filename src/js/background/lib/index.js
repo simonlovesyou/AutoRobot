944e3ce49b670fce0d0781f95349d74a17a32c95
simonlovesyou <simonljohansson92@gmail.com>
2016-04-12 22:33:03 +0200

    Add clipboard to userland

--- src/js/background/lib/index.js
+++ src/js/background/lib/index.js
@@ -2,10 +2,13 @@ import hotkey from './hotkey.js';
 import hotstring from './hotstring.js';
 import launch from './launch.js';
 import applescript from 'applescript';
+import {clipboard} from 'electron';
+
 
 module.exports = {
   hotkey,
   hotstring,
   launch,
-  applescript
+  applescript,
+  clipboard
 }
