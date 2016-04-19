6c28462ee6b375b005e4ed4b183f1c395bee55c6
simonlovesyou <simonljohansson92@gmail.com>
2016-04-19 23:06:31 +0200

    Add log to module exports

--- src/js/client/util/index.js
+++ src/js/client/util/index.js
@@ -1,10 +1,12 @@
 import scripts from './scripts';
 import shortcut from './shortcut';
 import setUserRequire from './require';
+import log from './log';
 
 
 module.exports = {
   scripts,
   setUserRequire,
-  shortcut
+  shortcut,
+  log
 }
