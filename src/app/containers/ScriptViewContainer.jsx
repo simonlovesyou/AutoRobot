ccba6373b396e80f18b1ab171ca758de3eeb74c2
simonlovesyou <simonljohansson92@gmail.com>
2016-04-21 15:23:28 +0200

    Add additional props to scriptview

--- src/app/containers/ScriptViewContainer.jsx
+++ src/app/containers/ScriptViewContainer.jsx
@@ -5,25 +5,28 @@ import { scripts } from '../actions/'
 const mapStateToProps = (state) => {
 
   let error = state.activeScript.error;
-  console.log("dispatching props");
-  console.log(state);
   return {
     src: state.activeScript.src || 'No active script src',
     content: state.activeScript.content || 'No active script code',
     status: state.activeScript.status || 'No active script',
     name: state.activeScript.name || 'No name',
+    code: state.activeScript.code || '',
+    logs: state.activeScript.logs || [],
     error
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  console.log(scripts);
   return {
     onRunClick: (src) => {
       dispatch(scripts.runScript(src))
     },
     onRefreshClick: (src) => {
       dispatch(scripts.refreshScript(src))
+    },
+    onScriptLog: (src) => scripts.log(dispatch, src),
+    onRemoveScript: (src) => {
+      dispatch(scripts.removeScript(src));
     }
   }
 }
