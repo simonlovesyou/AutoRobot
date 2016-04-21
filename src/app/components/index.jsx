9873091ca01307255264601b043c074f9caf2f6d
simonlovesyou <simonljohansson92@gmail.com>
2016-04-21 15:22:56 +0200

    Remove old ScriptFolder view

--- src/app/components/index.jsx
+++ src/app/components/index.jsx
@@ -1,5 +1,4 @@
 import Script from './Sidebar/Script.js';
-import ScriptFolder from './Sidebar/ScriptFolder.js';
 import ScriptList from './Sidebar/ScriptList.js';
 import ScriptView from './ScriptView/ScriptView.js';
 import App from './App/App.js';
