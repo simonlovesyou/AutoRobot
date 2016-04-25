ba37cfa8a4015fed918819a0f99810f505154cde
simonlovesyou <simonljohansson92@gmail.com>
2016-04-25 11:58:41 +0200

    Refactor code

--- src/app/components/index.jsx
+++ src/app/components/index.jsx
@@ -3,8 +3,6 @@ import ScriptList from './Sidebar/ScriptList.js';
 import ScriptView from './ScriptView/ScriptView.js';
 import App from './App/App.js';
 
-console.log(ScriptList);
-
 module.exports = {
   Script,
   ScriptList,
