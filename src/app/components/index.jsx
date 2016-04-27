b1d880df5ccbf789a45eb246b4ada0100e490496
simonlovesyou <simonljohansson92@gmail.com>
2016-04-27 19:46:47 +0200

    Expose AddScriptDialog component

--- src/app/components/index.jsx
+++ src/app/components/index.jsx
@@ -1,11 +1,13 @@
 import Script from './Sidebar/Script.js';
 import ScriptList from './Sidebar/ScriptList.js';
 import ScriptView from './ScriptView/ScriptView.js';
+import AddScriptDialog from './AddScriptDialog/AddScriptDialog.js';
 import App from './App/App.js';
 
 module.exports = {
   Script,
   ScriptList,
   App,
-  ScriptView
+  ScriptView,
+  AddScriptDialog
 }
